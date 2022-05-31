6e7d429e7a597a20c31c0c0d0f95731f9871200f
Volmius <85331545+Volmius@users.noreply.github.com>
2022-05-31 16:25:12 +0600

    Update ProjectConfig.java

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -9,7 +9,7 @@ import org.aeonbits.owner.Config;
         "classpath:config/remote.properties"
 })
 public interface ProjectConfig extends Config {
-    @Key("browser")
+       
     @DefaultValue("chrome")
     String browser();
     @DefaultValue("91.0")
